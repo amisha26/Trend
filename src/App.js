bb24d4dd9b14cf7b9dae2452e357a591021c8ea7
gamitej <gamiejpratapsingh998@gmail.com>
2021-08-26 22:08:13 +0530

    header moved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./comp/Header";
 import Main from "./comp/Main";
-import Product from "./comp/Product";
 import Footer from "./comp/Footer";
+import Login from "./comp/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <div className="header">
-          <Header />
-          <Main />
-        </div>
-        <Product />
         <Switch>
-          <Route path="/" />
+          <Route exact path="/" component={Main} />
+          <Route path="/login" component={Login} />
         </Switch>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
