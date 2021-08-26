d0eb4a839be16f98334155899101aabba7d7ba41
Amisha Tiwari <amisha26>
2021-08-26 22:27:45 +0530

    main added

--- src/comp/Main.js
+++ src/comp/Main.js
@@ -1,25 +1,31 @@
 import React from "react";
 import img1 from "../images/main.svg";
+import Header from "./Header";
+import Product from "./Product";
 
 const Main = () => {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-2">
-            <h1>
-              Give Your Workout <br />A New Style!
-            </h1>
-            <p>Get what is best for you.</p>
-            <a href="" className="btn">
-              Explore Now 
-            </a>
-          </div>
-          <div className="col-2">
-            <img src={img1} alt="#" />
+      <div className="header">
+        <Header />
+        <div className="container">
+          <div className="row">
+            <div className="col-2">
+              <h1>
+                Give Your Workout <br />A New Style!
+              </h1>
+              <p>Get what is best for you.</p>
+              <a href="" className="btn">
+                Explore Now
+              </a>
+            </div>
+            <div className="col-2">
+              <img src={img1} alt="#" />
+            </div>
           </div>
         </div>
       </div>
+      <Product />
     </div>
   );
 };
